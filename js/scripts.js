da9c8a63417c05106e49d1008499b5ae17904159
Nathan Mayer <nm.develops@gmail.com>
2017-05-24 16:32:18 -0700

    add simple js effects to scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,21 @@
 $(document).ready(function() {
-  $("h1").click(function() {
-    alert("This is a header.");
-  });
+  $(".clickable").click(function() {
+    //$("#walrus-showing").toggle();
+    //$("#walrus-hidden").toggle();
 
-  $("p").click(function() {
-    $(".walrus-showing").toggle();
-    $(".walrus-hidden").toggle();
-  });
+    //$("#walrus-showing").fadeIn();
+    //$("#walrus-hidden").toggle();
 
-  $("img").click(function() {
-    alert("This is an asshole.");
+    //$("#walrus-showing").fadeToggle();
+    //$("#walrus-hidden").fadeToggle();
+
+    //$("#walrus-showing").slideDown();
+    //$("#walrus-hidden").toggle();
+
+    //$("#walrus-showing").slideUp();
+    //$("#walrus-hidden").toggle();
+
+    $("#walrus-showing").slideToggle();
+    $("#walrus-hidden").slideToggle();
   });
 });
